b933370ab413004da066628cae9c7f88bb500023
pavanuppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2020-06-11 19:31:30 +0530

    Initial commit

---
+++ target/todo-web-application-h2/WEB-INF/classes/data.sql
@@ -0,0 +1,6 @@
+insert into TODO
+values(10001, 'AWS', false, sysdate(), 'pavanuppuluri');
+insert into TODO
+values(10002, 'PCF', false, sysdate(), 'pavanuppuluri');
+insert into TODO
+values(10003, 'Micro Services', false, sysdate(), 'pavanuppuluri');
